39cfb59e48c4d15a6c80724612531926ba138e82
Dylan Landry <dylanglandry@gmail.com>
2021-11-21 16:25:55 -0500

    add prisma with example schema

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+import type { User } from '@prisma/client'
 
-type Data = {
-  name: string
-}
+const prisma = new PrismaClient()
 
-export default function handler(
+type Data = User[]
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const allUsers = await prisma.user.findMany()
+  res.status(200).json(allUsers)
 }
